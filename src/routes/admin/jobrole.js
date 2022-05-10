2d809525cdc25b1f109a3b55ac1ef15a2e0d0196
jashwanthwebdev <81573204+jashwanthwebdev@users.noreply.github.com>
2022-05-10 10:53:33 +0530

    Add files via upload

--- src/routes/admin/jobrole.js
+++ src/routes/admin/jobrole.js
@@ -1,21 +1,28 @@
 const express = require('express');
 const jobrolesRoutes = express.Router();
 const multer = require('multer');    
+const { requireSigninAdmin } = require('../../commen-middleware/index'); 
+
+
 
 const { addjobrole} = require('../../controller/admin/jobrole'); 
-const { addselfassessment , editselfassessment, updateselfassessment} = require('../../controller/admin/selfassessment'); 
+const { addselfassessment ,getallselfassessmentcategory, addselfassessmentquestions, editselfassessment, updateselfassessment} = require('../../controller/admin/selfassessment'); 
 const storage = multer.diskStorage({ 
     destination: function (req, file, cb) { 
       cb(null, path.join(path.dirname(__dirname), "uploads"));
     },
     filename: function (req, file, cb) {
-      cb(null, shortid.generate() + "-" + file.originalname);  
+      cb(null, shortid.generate() + "-" + file.originalname);   
     }, 
   });  
 let upload = multer({ storage }); 
-jobrolesRoutes.post('/addjobrole', upload.none(), addjobrole);   
+jobrolesRoutes.post('/addjobrole', upload.none(), addjobrole);    
   
-jobrolesRoutes.post('/addselfassessment',upload.none(),addselfassessment);  
+jobrolesRoutes.post('/self/addselfassessmentquestions',requireSigninAdmin,upload.none(),addselfassessmentquestions);  
+jobrolesRoutes.post('/addselfassessment',upload.none(),addselfassessment);   
+jobrolesRoutes.get('/getallselfassessmentcategory',requireSigninAdmin,getallselfassessmentcategory)
+
+
 jobrolesRoutes.get('/editselfassessment/:id', editselfassessment)   
 jobrolesRoutes.put('/updateselfassessment/:id',upload.none(),updateselfassessment)  
 module.exports = jobrolesRoutes;        
